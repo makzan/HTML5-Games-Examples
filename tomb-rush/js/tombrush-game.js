1687ac5ebd3c5b74e1a10fa93176240f1c4706be
Thomas Seng Hin Mak <makzan@gmail.com>
2012-09-27 00:20:39 +0800

    hero can drop and jump

--- tomb-rush/js/tombrush-game.js
+++ tomb-rush/js/tombrush-game.js
@@ -16,6 +16,8 @@ tombrush.CommonShapes = (function() {
   CommonShapes.rectangle = function(rect) {    
     if (rect.strokeColor === undefined) rect.strokeColor = "#000";
     if (rect.fillColor === undefined) rect.fillColor = "#000";
+    if (rect.x === undefined) rect.x = 0;
+    if (rect.y === undefined) rect.y = 0;
     if (rect.width === undefined) rect.width = 0;
     if (rect.height === undefined) rect.height = 0;
     
@@ -36,7 +38,7 @@ tombrush.CommonShapes = (function() {
 
 tombrush.Tile = (function(){  
   function Tile(number){       
-    this.initialize();
+    this.initialize(); // initializing the createjs Container
 
     this.number = number;
 
@@ -55,6 +57,48 @@ tombrush.Tile = (function(){
   return Tile;
 })();
 
+tombrush.Hero = (function(){
+  function Hero() {
+    this.initialize(); // initializing the createjs Container
+  };
+  var p = Hero.prototype = new createjs.Container();
+
+  // Public properties
+  p.dropSpeed = 1;
+  p.velocity = new createjs.Point(0, 0);
+
+  
+  // super initialize
+  p.Container_initialize = p.initialize;
+  p.initialize = function() {
+    this.Container_initialize(); // super init
+
+    this.width = 10;
+    this.height = 20;
+
+    var shape = tombrush.CommonShapes.rectangle({
+      width: this.width,
+      height: this.height,
+      fillColor: "#f00",
+    });
+    this.addChild(shape);
+
+    // Give heartbeat to Hero
+    createjs.Ticker.addListener(this, /*pausable=*/ true);
+  }
+
+  p.tick = function(timeElapsed) {
+    this.velocity.y += this.dropSpeed;
+    this.y += this.velocity.y;
+  };
+
+  p.jump = function() {
+    this.velocity.y = -10;
+  };
+
+  return Hero;
+})();
+
 tombrush.Game = (function() {    
   // constructor  
   function TombRushGame() {
@@ -68,6 +112,10 @@ tombrush.Game = (function() {
     // Enabling the Touches on mobile device
     createjs.Touch.enable(this.stage, /*singleTouch=*/ true, /*allowDefault=*/false);
 
+    // Create heartbreat for our game loop
+    createjs.Ticker.setFPS(40);
+    createjs.Ticker.addListener(this, /*pausable=*/ true);
+
     this.initGame();       
   }  
 
@@ -75,12 +123,23 @@ tombrush.Game = (function() {
 
   p.initGame = function() {
     
-    
+    var hero = new tombrush.Hero();
+    hero.x = 100;
+    hero.y = 100;
+    this.stage.addChild(hero);
+
+    this.stage.onMouseUp = function() {      
+      hero.jump();
+    }
+
     this.updateView();
   };
 
-  p.updateView = function() {
-    this.nextTileLabel.innerText = this.nextCount;
+  p.tick = function() {
+    this.updateView();
+  }
+
+  p.updateView = function() {    
     this.stage.update();
   };
   
