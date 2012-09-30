d47bfa3e5b1427332569fdd342fcd413dcd01652
Thomas Seng Hin Mak <makzan@gmail.com>
2012-09-30 13:06:39 +0800

    Still working on new collision approach

--- tomb-rush/js/tombrush-game.js
+++ tomb-rush/js/tombrush-game.js
@@ -56,12 +56,23 @@ tombrush.GameObject = (function(){
 
   // rectangle bounding box collision checking
   p.hitGameObject = function(gameObj) {
-    if (this.x >= gameObj.x + gameObj.width
-     || this.x + this.width <= gameObj.x
-     || this.y >= gameObj.y + gameObj.height
-     || this.y + this.height <= gameObj.y)
-      return false;
-    return true;
+    var halfW1 = this.width/2;    
+    var halfH1 = this.height/2;
+    var centerX1 = this.x + halfW1;
+    var centerY1 = this.y + halfH1;
+
+    var halfW2 = gameObj.width/2;
+    var halfH2 = gameObj.height/2;
+    var centerX2 = gameObj.x + halfW2;
+    var centerY2 = gameObj.y + halfH2;
+
+    var deltaX = Math.abs(centerX1 - centerX2) - (halfW1 + halfW2);
+    var deltaY = Math.abs(centerY1 - centerY2) - (halfH1 + halfH2);
+
+    if (deltaX < 0 && deltaY < 0) {
+      return {deltaX:deltaX, deltaY:deltaY};
+    }
+    return null;
   };
   
   // TODO: this method should detect which side the game object hits the other object.
@@ -72,22 +83,41 @@ tombrush.GameObject = (function(){
     this.touching = 0x0000; // reset first
     
     // top    
-    if (this.y <= gameObj.y + gameObj.height && this.y + this.height > gameObj.y) {
-      this.touching += 0x1000;
-     // gameObj.touching
-    }
+   //  if (this.y <= gameObj.y + gameObj.height && this.y + this.height > gameObj.y) {
+   //    this.touching = this.touching | 0x1000;
+   //    gameObj.touching = gameObj.touching | 0x0010;
+   //  }
     
-    // bottom
-//    if (this.y < gameObj.y + gameObj.height && this.y + this.height > gameObj.y)
+   //  // bottom
+   // if (this.y < gameObj.y + gameObj.height && this.y + this.height > gameObj.y)
   }  
 
+  // p.willHitGameObject = function(gameObj) {
+  //   if (this.projectedX >= gameObj.projectedX + gameObj.width
+  //    || this.projectedX + this.width <= gameObj.projectedX
+  //    || this.projectedY >= gameObj.projectedY + gameObj.height
+  //    || this.projectedY + this.height <= gameObj.projectedY)
+  //     return false;
+  //   return true;
+  // };
   p.willHitGameObject = function(gameObj) {
-    if (this.projectedX >= gameObj.projectedX + gameObj.width
-     || this.projectedX + this.width <= gameObj.projectedX
-     || this.projectedY >= gameObj.projectedY + gameObj.height
-     || this.projectedY + this.height <= gameObj.projectedY)
-      return false;
-    return true;
+    var halfW1 = this.width/2;    
+    var halfH1 = this.height/2;
+    var centerX1 = this.projectedX + halfW1;
+    var centerY1 = this.projectedY + halfH1;
+
+    var halfW2 = gameObj.width/2;
+    var halfH2 = gameObj.height/2;
+    var centerX2 = gameObj.projectedX + halfW2;
+    var centerY2 = gameObj.projectedY + halfH2;
+
+    var deltaX = Math.abs(centerX1 - centerX2) - (halfW1 + halfW2);
+    var deltaY = Math.abs(centerY1 - centerY2) - (halfH1 + halfH2);
+
+    if (deltaX < 0 && deltaY < 0) {
+      return {deltaX:deltaX, deltaY:deltaY};
+    }
+    return null;
   };
 
   // TODO: there are two types of platform, 
@@ -105,11 +135,13 @@ tombrush.GameObject = (function(){
       // ignore this object itself
       if (this.name === gameObj.name) continue;      
 
-      if(this.hitGameObject(gameObj)) {
-        return gameObj;
+      var delta = this.hitGameObject(gameObj);
+      if(delta != null) {
+        delta.gameObj = gameObj;
+        return delta;
       }
     }
-    return undefined;
+    return null;
   }
 
   p.willHitAnyGameObject = function() {
@@ -119,11 +151,13 @@ tombrush.GameObject = (function(){
       // ignore this object itself
       if (this.name === gameObj.name) continue;      
 
-      if(this.willHitGameObject(gameObj)) {
-        return gameObj;
+      var delta = this.willHitGameObject(gameObj)
+      if(delta != null) {
+        delta.gameObj = gameObj;
+        return delta;
       }
     }
-    return undefined;
+    return null;
   }
 
   return GameObject;
@@ -195,14 +229,23 @@ tombrush.Hero = (function(){
 
   p.maxVelocityUntilHit = function () {
     var maxVelocity = this.velocity.clone();
+
+    // var hit = this.willHitAnyGameObject();
+    // if (hit !== null) {
+    //   maxVelocity.y -= hit.deltaY;
+    //   this.velocity.y = maxVelocity.y;
+    // }
+    // console.log ('max velocity: ', maxVelocity.y);
     
-    // Y axis
+
+    // Y axis    
+    // maxVelocity.y = hitDelta.deltaY    
     if (this.velocity.y > 0) {
       for (var i=1;i<=this.velocity.y;i++) {      
         maxVelocity.y = i;
         this.projectedY = this.y + maxVelocity.y;
         var hit = this.willHitAnyGameObject();
-        if (hit !== undefined) {
+        if (hit !== null) {
           maxVelocity.y -= 1; 
           this.velocity.y = maxVelocity.y;
           break;
@@ -213,16 +256,16 @@ tombrush.Hero = (function(){
         maxVelocity.y = i;
         this.projectedY = this.y + maxVelocity.y;
         var hit = this.willHitAnyGameObject();
-        if (hit !== undefined) { 
+        if (hit !== null) { 
           maxVelocity.y += 1;
           this.velocity.y = maxVelocity.y;
           break;
         }
       }
     }
-    
+        
     // X axis
-    if (this.velocity.x > 0) {
+    /*if (this.velocity.x > 0) {
       for (var i=1;i<=this.velocity.x; i++) {
         maxVelocity.x = i;
         this.projectedX = this.x + maxVelocity.x;
@@ -244,7 +287,7 @@ tombrush.Hero = (function(){
           break;
         }
       }
-    }
+    }*/
     
     return maxVelocity;
   }
